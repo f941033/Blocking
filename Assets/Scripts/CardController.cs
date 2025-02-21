c39e6752e88f09ad4d83214df642b134bb1ba322
f941033 <f941033@gmail.com>
2025-02-21 20:31:25 +0100

    pulido

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -13,7 +13,7 @@ public class CardController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        visorTraje.SetActive(false);
     }
 
     // Update is called once per frame
