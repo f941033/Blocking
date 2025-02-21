677620332c8b7c1a95e4739217c5e123656d672a
f941033 <f941033@gmail.com>
2025-02-21 10:09:02 +0100

    programación puertas

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    bool yellowCard = false;
+    bool orangeCard = false;
+    public GameObject yellowDoor, orangeDoor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "YellowCard")
+        {
+            yellowCard = true;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "YellowDoor")
+        {
+            if (yellowCard)
+            {
+                yellowDoor.GetComponent<Animator>().Play("AbrirPuerta");    
+            }
+        }
+
+
+
+        if (other.tag == "OrangeCard")
+        {
+            orangeCard = true;
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "OrangeDoor")
+        {
+            if(orangeCard)
+            {
+                orangeDoor.GetComponent<Animator>().Play("AbrirPuertaNaranja");
+            }
+        }
+    }
+}
